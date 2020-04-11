6d3ab7fd07c54819abc4a5c9a7d556ee8eb43a73
Trick <petropatrickj@gmail.com>
2020-04-11 19:56:44 -0400

    adding build file

---
+++ build/precache-manifest.a150f4c8a2e25e24cd5ae1f1a7c1c68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73c0d6dd3500c813a65c415e5eff251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f166dc733aa76cc52f",
+    "url": "/static/css/main.67548d04.chunk.css"
+  },
+  {
+    "revision": "6ccd28f805bd61371fea",
+    "url": "/static/js/2.57354e07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57354e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f166dc733aa76cc52f",
+    "url": "/static/js/main.d1a7d386.chunk.js"
+  },
+  {
+    "revision": "457698f1677daef0ae8c",
+    "url": "/static/js/runtime-main.7d861018.js"
+  }
+]);
