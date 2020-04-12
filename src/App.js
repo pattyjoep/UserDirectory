3fd3a42448d823cc9affa03c15b22647808c446f
Trick <petropatrickj@gmail.com>
2020-04-11 22:34:34 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Directory from "./pages/Directory";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
